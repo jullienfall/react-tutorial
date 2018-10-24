3f5c3c96d86758b4a1df12c81dc030d985fbe1e4
jullienfall <jullien.fall@gmail.com>
2018-10-25 00:34:48 +0300

    done

--- src/board.js
+++ src/board.js
@@ -3,15 +3,12 @@ import { Square } from './square';
 
 export class Board extends Component {
   renderSquare(i) {
-    return <Square data={i} />;
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
